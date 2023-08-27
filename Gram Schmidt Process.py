5527d4bfab324be4424ac24927f4500448cdfcb7
chiefaj <abhijeet.solanki@outlook.com>
2023-08-27 00:22:04 -0500

    Added output as comment in Gram Schmidt process.pt

--- Gram Schmidt Process.py
+++ Gram Schmidt Process.py
@@ -26,3 +26,9 @@ u2 = w2 / np.linalg.norm(w2)
 print("Orthonormal Vectors u1 and u2 Using Gram Schmidt Process:")
 print("u1:", u1)
 print("u2:", u2)
+
+# Output:
+# (base) chiefaj@Abhijeets-MacBook-Pro CSC-6903 % /Users/chiefaj/miniconda3/envs/.env/bin/python "/Users/chiefaj/Tntech-Masters/CSC-6903/Gram Schmidt Process.py"
+# Orthonormal Vectors u1 and u2 Using Gram Schmidt Process:
+# u1: [0.70710678 0.70710678]
+# u2: [-0.70710678  0.70710678]
